63b874be8e697f60825e70f62beeb43fdbdb2595
JuYoung Park <43464907+park-ju1008@users.noreply.github.com>
2019-01-08 16:17:48 +0900

    Add files via upload

---
+++ EmptyRecyclerView.java
@@ -0,0 +1,66 @@
+package com.info.idol.community.custom;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class EmptyRecyclerView extends RecyclerView {
+    private View emptyView;
+
+    public EmptyRecyclerView(Context context) {
+        super(context);
+    }
+
+    public EmptyRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EmptyRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    private void initEmptyView(){
+        if(emptyView!=null){
+            emptyView.setVisibility(getAdapter()==null||getAdapter().getItemCount()==0? VISIBLE:GONE);
+            EmptyRecyclerView.this.setVisibility(getAdapter()==null||getAdapter().getItemCount()==0?GONE:VISIBLE);
+        }
+    }
+
+    final AdapterDataObserver observer=new AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            initEmptyView();
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            super.onItemRangeInserted(positionStart, itemCount);
+            initEmptyView();
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            super.onItemRangeRemoved(positionStart, itemCount);
+            initEmptyView();
+        }
+    };
+
+    @Override
+    public void setAdapter(Adapter adapter) {
+        Adapter oldAdapter = getAdapter();
+        super.setAdapter(adapter);
+        if(oldAdapter!=null){
+            oldAdapter.unregisterAdapterDataObserver(observer);
+        }
+        if(adapter!=null){
+            adapter.registerAdapterDataObserver(observer);
+        }
+    }
+    public void setEmptyView(View view){
+        this.emptyView=view;
+        initEmptyView();
+    }
+}
